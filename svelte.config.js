b4bd2b98df76373057f7b46d4b122734f74c5883
palaashnaik <68655536+palaashnaik@users.noreply.github.com>
2024-12-02 00:23:06 +0530

    going back to the previous commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         adapter: adapter({
-            runtime: 'nodejs20'
+            runtime: 'edge'
         })
     },
     preprocess: vitePreprocess()
